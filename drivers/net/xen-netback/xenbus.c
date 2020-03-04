e5a02ac3d659b1f345916f77e554c157ff27eeb8
Chong-Li <lichong659@gmail.com>
2020-03-03 19:27:43 -0600

    new

--- drivers/net/xen-netback/xenbus.c
+++ drivers/net/xen-netback/xenbus.c
@@ -202,6 +202,27 @@ static int netback_uevent(struct xenbus_device *xdev,
 	return add_uevent_var(env, "vif=%s", be->vif->dev->name);
 }
 
+static int xen_net_read_mac(struct xenbus_device *dev, u8 mac[])
+{
+	char *s, *e, *macstr;
+	int i;
+
+	macstr = s = xenbus_read(XBT_NIL, dev->nodename, "mac", NULL);
+	if (IS_ERR(macstr))
+		return PTR_ERR(macstr);
+
+	for (i = 0; i < ETH_ALEN; i++) {
+		mac[i] = simple_strtoul(s, &e, 16);
+		if ((s == e) || (*e != ((i == ETH_ALEN-1) ? '\0' : ':'))) {
+			kfree(macstr);
+			return -ENOENT;
+		}
+		s = e+1;
+	}
+
+	kfree(macstr);
+	return 0;
+}
 
 static void backend_create_xenvif(struct backend_info *be)
 {
@@ -339,27 +360,6 @@ static void xen_net_read_rate(struct xenbus_device *dev,
 	kfree(ratestr);
 }
 
-static int xen_net_read_mac(struct xenbus_device *dev, u8 mac[])
-{
-	char *s, *e, *macstr;
-	int i;
-
-	macstr = s = xenbus_read(XBT_NIL, dev->nodename, "mac", NULL);
-	if (IS_ERR(macstr))
-		return PTR_ERR(macstr);
-
-	for (i = 0; i < ETH_ALEN; i++) {
-		mac[i] = simple_strtoul(s, &e, 16);
-		if ((s == e) || (*e != ((i == ETH_ALEN-1) ? '\0' : ':'))) {
-			kfree(macstr);
-			return -ENOENT;
-		}
-		s = e+1;
-	}
-
-	kfree(macstr);
-	return 0;
-}
 
 static void unregister_hotplug_status_watch(struct backend_info *be)
 {
